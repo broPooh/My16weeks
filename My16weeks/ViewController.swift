26d01037fa339d82672efc12cb3122faae2e2b15
broMac <yhs4705@naver.com>
2022-06-16 10:34:13 +0900

    Initial Commit

---
+++ My16weeks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My16weeks
+//
+//  Created by bro on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
